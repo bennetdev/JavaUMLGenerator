c59a1caf0bca8aac270f4879d9b98896336126fc
Bennet <bennetm999@gmail.com>
2020-09-24 22:37:09 +0200

    create uml on canvas in specific position

--- GUI.py
+++ GUI.py
@@ -5,6 +5,7 @@ import sys
 import threading
 from UMLKlasse import UMLKlasse
 
+
 class View:
     def __init__(self, master, width, height):
         self.width = width
@@ -14,17 +15,15 @@ class View:
         self.display_size_index = -1
 
         master.title("UML Generator")
-        self.master.resizable(False, False)
         self.frame = tk.Frame(self.master, width=width, height=height)
         self.frame.pack(expand=True)
         self.canvas = tk.Canvas(self.frame, width=400, height=400, scrollregion=(0, 0, 0, 500), bg="#ffffff")
         self.canvas.pack()
-        self.canvas.create_line(15, 25, 200, 25)
         uml = UMLKlasse({
-        "variables": ["var5", "var6"],
-        "methods": ["method5", "method6"]
-    }, self.canvas, "klasse1")
-        uml.create_single_class_rect(10,10)
+            "variables": ["String var5", "int var6", "double var7"],
+            "methods": ["String method5", "void method6"]
+        }, self.canvas, "Klasse1")
+        uml.create_single_class_rect(100, 100)
 
 
 root = tk.Tk()
--- UMLKlasse.py
+++ UMLKlasse.py
@@ -5,24 +5,27 @@ class UMLKlasse:
         self.canvas = canvas
         self.classname = classname
 
+
     def draw(self):
         pass
     def create_single_class_rect(self, offset_x, offset_y):
         self.canvas.create_text(55 + offset_x, 20 + offset_y, text=self.classname)
-        self.canvas.create_line(10 + offset_x, 25 + offset_y, 110 + offset_x, 25 + offset_y)
+        self.canvas.create_line(10 + offset_x, 25 + offset_y, 150 + offset_x, 25 + offset_y)
         # value for start of line separating variables and methods
         separator_y = 0
         # iterate over variable-names in current class
         for variable_index, variable_name in enumerate(self.class_dictionary["variables"]):
-            separator_y = 35 + 5 * variable_index
-            self.canvas.create_text(20 + offset_x, separator_y + offset_y, text=variable_name)
+            separator_y = 35 + 15 * variable_index
+            self.canvas.create_text(20 + offset_x, separator_y + offset_y, text=variable_name, anchor="w")
         # generate sepator line if there are variables
         if separator_y != 0:
-            self.canvas.create_line(10 + offset_x, separator_y + 5 + offset_y, 110 + offset_x, separator_y + 5 + offset_y)
+            self.canvas.create_line(10 + offset_x, separator_y + 10 + offset_y, 150 + offset_x, separator_y + 10 + offset_y)
         rectangle_end_y = separator_y
         # iterate over method-names in current class
         for method_index, method_name in enumerate(self.class_dictionary["methods"]):
-            rectangle_end_y = (separator_y + 15) + 5 * method_index
-            self.canvas.create_text(20 + offset_x, rectangle_end_y + offset_y, text=method_name)
+            rectangle_end_y = (separator_y + 20) + 15 * method_index
+            print(rectangle_end_y)
+            self.canvas.create_text(20 + offset_x, rectangle_end_y + offset_y, text=method_name, anchor="w")
         # create rectangle for class in specific size
-        self.canvas.create_rectangle(10 + offset_x, 10 + offset_y, 100, rectangle_end_y - 5)
+
+        self.canvas.rect = self.canvas.create_rectangle(10 + offset_x, 10 + offset_y, 150 + offset_x, rectangle_end_y + offset_y + 10)
